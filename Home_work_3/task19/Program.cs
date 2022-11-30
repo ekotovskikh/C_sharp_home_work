f604b4fd3dbc2528c1177a66f47a76cc4d8df9da
Helen <lena_gl83@mail.ru>
2022-11-30 12:46:29 +0300

    HomeW3

---
+++ Home_work_3/task19/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Clear(); 
+
+Console.Write("Введите 5-и значное число: "); 
+
+int n = Convert.ToInt32(Console.ReadLine()); // ввод числа 
+
+if (n/10000 == n % 10 && ((n/1000) % 10) == ((n / 10) % 10)) 
+{     Console.WriteLine("Это число вяляется полиндромом"); 
+} else 
+{     
+   Console.WriteLine("Это число не является полиндромом"); 
+   }
+
+
+
+
+
+
+
+//if (n < 10000)     
+//Console.WriteLine("Вы ошиблись, введите 5-и значное число!"); 
+
+//else 
+//{    
+   
+     //{         
+     //Console.WriteLine("Да, число является полиндромом"); 
+    //} 
+    // else
+    //{
+       // Console.WriteLine("Нет, число не является полиндромом");
+    // }
+   // } 
---
+++ Home_work_3/task23/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear(); 
+Console.Write("Ведите число: "); 
+int n = Convert.ToInt32(Console.ReadLine()); 
+for (int i = 1; i <= n; i++)     
+Console.Write($"{i * i * i} ");
+
