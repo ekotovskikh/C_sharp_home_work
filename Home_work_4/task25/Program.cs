deef8238def5aa24b3850de30b9d27a4b80c0e2d
Helen <lena_gl83@mail.ru>
2022-12-08 01:31:58 +0300

    added HW_4

---
+++ Home_work_4/task25/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+//НЕ ИСПОЛЬЗОВАТЬ MATH.POW
+//3, 5 -> 243 (3⁵) 2, 4 -> 16
+
+Console.Clear(); 
+Console.Write("Введите число A: "); 
+int a = Convert.ToInt32(Console.ReadLine()); 
+Console.Write("Введите число B: "); 
+int b = Convert.ToInt32(Console.ReadLine());
+
+int degree = a;
+
+for (int i = 1; i < b; i++)
+{
+degree= degree*a;
+}
+
+Console.WriteLine("Число А в степени В = " + degree);
---
+++ Home_work_4/task29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив 
+//из 8 элементов, предоставить возможность пользователю ввести числа и вывести их на экран.
+//1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+Console.Clear(); 
+
+int[] array = new int[8]; 
+for (int i = 0; i < 8; i++) 
+{     
+    Console.Write("Введите элемент массива: ");     
+    int x = Convert.ToInt32(Console.ReadLine());     
+    array[i] = x; 
+} 
+    Console.WriteLine($"[{string.Join(", ", array)}]");
+
+
+
+
+
+
+
+//for (int i = 0; i < 8; i++)     
+//array[i] = new Random().Next(, ); 
+//Console.WriteLine($"Результат: [{string.Join(", ", array)}]");
