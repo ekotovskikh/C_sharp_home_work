d64968b7c3d93cf8b3bc615c941264e2be38f6ec
Helen <lena_gl83@mail.ru>
2022-12-12 01:28:45 +0300

    added Home_work_6

---
+++ Home_work_6/task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+ Console.Clear();
+
+Console.Write("Введите элементы через пробел, обязательно: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);// это кончно подсмотрела на просторах, был изнасально более ёмкий вариант
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Количество элементов больше 0: {count}");
+
+
+//Console.Clear();
+ //Console.Write("Ведите  число M1: "); 
+ //int n = Convert.ToInt32(Console.ReadLine());  
+ //Console.Write("Ведите  число M2: "); 
+ //int n1 = Convert.ToInt32(Console.ReadLine()); 
+ //Console.Write("Ведите  число M3: "); 
+ //int n2 = Convert.ToInt32(Console.ReadLine()); 
+ //Console.Write("Ведите  число M4: "); 
+ //int n3 = Convert.ToInt32(Console.ReadLine()); 
+ //Console.Write("Ведите  число M5: "); 
+ //int n4 = Convert.ToInt32(Console.ReadLine()); 
+
+//int count=0;
+     
---
+++ Home_work_6/task43/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+//заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+//b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.Clear(); 
+
+Console.Write("Ведите значение b1: "); 
+double b1 = Convert.ToDouble(Console.ReadLine()); 
+Console.Write("Ведите значение b2: "); 
+double b2 = Convert.ToDouble(Console.ReadLine()); 
+Console.Write("Ведите значение k1: "); 
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Ведите значение k2: "); 
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+Console.Write($"Точка пересечения двух прямых: {(k1*(b2-b1)/(k1-k2)+b1)} ");
